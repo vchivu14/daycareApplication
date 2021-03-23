3bb2dd9b9826dad90c1a61e2b5122125b46ae94f
vchivu14 <chivu.vlad@yahoo.com>
2021-03-23 20:08:00 +0100

    "stable-version-again"

--- src/main/java/com/standbystill/managementdaycare/controllers/ChildrenController.java
+++ src/main/java/com/standbystill/managementdaycare/controllers/ChildrenController.java
@@ -2,6 +2,8 @@ package com.standbystill.managementdaycare.controllers;
 
 import com.standbystill.managementdaycare.entities.Address;
 import com.standbystill.managementdaycare.entities.Child;
+import com.standbystill.managementdaycare.entities.Parent;
+import com.standbystill.managementdaycare.entities.Person;
 import com.standbystill.managementdaycare.services.AddressCRUDService;
 import com.standbystill.managementdaycare.services.ChildrenCRUDService;
 import com.standbystill.managementdaycare.services.PersonCRUDService;
@@ -22,7 +24,7 @@ public class ChildrenController {
     @Autowired
     AddressCRUDService addressCRUDService;
 
-    @GetMapping("/families/{idF}/children/{idC}")
+    @GetMapping("/families/{idF}/child/{idC}")
     public String showChildrenForFamily(@PathVariable("idF") int idF, @PathVariable("idC") int idC, Model model) {
         model.addAttribute("familyId", idF);
         model.addAttribute("childId", idC);
@@ -30,7 +32,7 @@ public class ChildrenController {
         return "child";
     }
 
-    @GetMapping("/families/{idF}/children/{idC}/delete")
+    @GetMapping("/families/{idF}/child/{idC}/delete")
     public String deleteChild(@PathVariable("idF") int idF, @PathVariable("idC") int idC) {
         Boolean delete = childrenCRUDService.deleteChild(idC);
         if (delete) {
@@ -40,41 +42,61 @@ public class ChildrenController {
         }
     }
 
-    @GetMapping("/families/{idF}/address")
+    @GetMapping("/families/{idF}/address/child")
     public String retrieveAddressModel(@PathVariable("idF") int idF, Model model) {
         model.addAttribute("familyID", idF);
         model.addAttribute("address", new Address());
-        return "formAddressP";
+        return "formAddressC";
     }
 
-    @PostMapping("/families/{idF}/address")
+    @PostMapping("/families/{idF}/address/child")
     public String addAddress(@PathVariable("idF") int idF, @ModelAttribute Address address, Model model) {
         model.addAttribute("familyID", idF);
         int addressId = addressCRUDService.addAddress(address);
         model.addAttribute("address", address);
         model.addAttribute("addressId", addressId);
-        return "resultAddressP";
+        return "resultAddressC";
     }
 
+    @GetMapping("/families/{idF}/address/{idA}/person/child")
+    public String retrievePersonModel(@PathVariable("idF") int idF, @PathVariable("idA") int idA, Model model) {
+        model.addAttribute("familyID", idF);
+        model.addAttribute("addressId", idA);
+        model.addAttribute("person", new Person());
+        return "formPersonC";
+    }
 
+    @PostMapping("/families/{idF}/address/{idA}/person/child")
+    public String addPerson(@PathVariable("idF") int idF, @PathVariable("idA") int idA,
+                            @ModelAttribute Person person, Model model) {
+        model.addAttribute("familyID", idF);
+        model.addAttribute("addressId", idA);
+        int personId = personCRUDService.addPerson(person, idA);
+        model.addAttribute("person", person);
+        model.addAttribute("personId", personId);
+        return "resultPersonC";
+    }
 
+    @GetMapping("/families/{idF}/address/{idA}/person/{idP}/child")
+    public String retrieveChildModel(@PathVariable("idF") int idF, @PathVariable("idA") int idA,
+                                      @PathVariable("idP") int idP, Model model) {
+        model.addAttribute("familyID", idF);
+        model.addAttribute("addressId", idA);
+        model.addAttribute("personId", idP);
+        model.addAttribute("child", new Child());
+        return "formChild";
+    }
 
-
-
-//    @GetMapping("/families/{id}/children/add")
-//    public String retrieveChildModel(@PathVariable("id") int id, Model model) {
-//        model.addAttribute("child", new Child());
-//        model.addAttribute("familyId", id);
-//        return "formChild";
-//    }
-//
-//    @PostMapping("/families/{id}/children/add")
-//    public String addChild(@PathVariable("id") int id, @ModelAttribute Child child, Model model) {
-//        int childId = childrenCRUDService.addChild(child, id);
-//        model.addAttribute("familyId", id);
-//        model.addAttribute("childId", childId);
-//        model.addAttribute("child", child);
-//        return "redirect:/families/"+id;
-//    }
+    @PostMapping("/families/{idF}/address/{idA}/person/{idP}/child")
+    public String addChild(@PathVariable("idF") int idF, @PathVariable("idA") int idA, @PathVariable("idP") int idP,
+                            @ModelAttribute Child child, Model model) {
+        model.addAttribute("familyID", idF);
+        model.addAttribute("addressId", idA);
+        model.addAttribute("personId", idP);
+        model.addAttribute("child", child);
+        int childId = childrenCRUDService.addChild(child,idF,idP);
+        model.addAttribute("childId", childId);
+        return "resultChild";
+    }
 
 }
